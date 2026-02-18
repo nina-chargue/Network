2980ed82f6c41fc25a5284f9d570616d28eefed4
nina-chargue <nina8chargue@gmail.com>
2026-02-17 22:22:21 -0600

    Updated startup and yaml files

--- startup.sh
+++ startup.sh
@@ -1,1 +1,5 @@
-python manage.py collecstatic && gunicorn --workers 2 project4.wsgi
+#!/bin/sh
+set -e
+
+gunicorn project4.wsgi:application --bind 0.0.0.0:8080 
+# --workers 2 --threads 3 --timeout 120
